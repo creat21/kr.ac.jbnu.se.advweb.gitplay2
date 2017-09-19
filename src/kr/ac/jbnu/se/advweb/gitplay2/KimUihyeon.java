98b9a2abee044c7658af169a2bbbb3f60d74a3e1
creat21 <sungu9496@naver.com>
2017-09-19 21:52:51 +0900

    aa

--- src/kr/ac/jbnu/se/advweb/gitplay2/KimUihyeon.java
+++ src/kr/ac/jbnu/se/advweb/gitplay2/KimUihyeon.java
@@ -2,7 +2,7 @@ package kr.ac.jbnu.se.advweb.gitplay2;
 #incldu
 public class KimUihyeon {
 	public static void main(String[] args) {
-		System.out.println("안녕하세요");
+		System.out.println("hello");
 		
 	}
 }
